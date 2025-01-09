9bc35dd04a33e0dd72488dd1b727c12b329ed2e2
Milkanzs23 <nzosabamilka@gmail.com>
2025-01-09 09:04:47 +0100

    modification 2

--- controllers/system/system.controller.js
+++ controllers/system/system.controller.js
@@ -83,9 +83,11 @@ const getAvenues = async (req, res) => {
 
 const getTest = async (req,res) =>{
     try {
+        const { collineId } = req.params
+        
 
     }catch (error){
-        
+
     }
 }
 
